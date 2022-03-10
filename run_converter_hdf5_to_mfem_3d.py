8f6551a4ad0392b4f5a9613e14801a46433f4778
prbansal <pratyuksh.bansal@gmail.com>
2022-03-10 13:37:33 +0100

    Fix 3d converter from hdf5 to MFEM mesh format

--- run_converter_hdf5_to_mfem_3d.py
+++ run_converter_hdf5_to_mfem_3d.py
@@ -1,39 +1,99 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
+
 import converters.geometry as gm
 import converters.converter_hdf5_to_mfem as mconv
 import generators.io_mesh as meshio
 
 
-def call_mesh_hdf5_to_mfem_3d():
+# Base input mesh directory
+base_in_mesh_dir = "./meshes/"
+
+# Base output mesh directory
+base_out_mesh_dir = "./meshes_MFEM/"
+
+# types of bisection-tree refined meshes
+br_type = ["deg0", "deg1", "deg2", "deg3"]
+
+# types of graded meshes
+gr_type = ["weight3E-1", "weight1E-1", "weight4E-2"]
+
+
+def call_converter_hdf5_to_mfem_3d(geometry_type, mesh_type, level, deg):
     
-    xFem_type = ["linear", "quadratic", "cubic"]
-    graded_type = ["3e-1", "2e-1", "1e-1"]
+    print(geometry_type, mesh_type, level, deg)
     
-    l = 7
-    idx = 2
+    geom_file = None
+    h5_mesh_file = None
+    mfem_mesh_file = None
     
-    geom_file = "geometry_files/prism.txt"
-    h5_mesh_file = "meshes/prism_graded/weight_5e-1/mesh_l%d.h5"%l
-    mfem_mesh_file = "meshes_MFEM/prism_graded/weight_5e-1/mesh_l%d.mesh"%l
-    
-    #geom_file = "geometry_files/prism.txt"
-    #h5_mesh_file = "meshes/prism_graded/weight_"+graded_type[idx]+"/mesh_l%d.h5"%l
-    #mfem_mesh_file = "meshes_MFEM/prism_graded/weight_"+graded_type[idx]+"/mesh_l%d.mesh"%l
-    
+    # Unit-cube geometry
+    if geometry_type in ['unit-cube', 'unit_cube', 'unitCube']:
+        
+        geom_file = "geometry_files/unitCube.txt"
+
+        if mesh_type in ['qu', 'quasi-uniform', 'quasi_uniform', 'quasiUniform']:
+        
+            h5_mesh_file = base_in_mesh_dir+"unitCube_qu/mesh_l%d.h5"%level
+            mfem_mesh_file = base_out_mesh_dir+"unitCube_qu/mesh_l%d.mesh"%level
+        
+        else:
+            print("Unknown mesh type!")
+            sys.exit()
+        
+     # Fichera cube geometry
+    elif geometry_type in ['fichera-cube', 'fichera_cube', 'ficheraCube']:
+        
+        geom_file = "geometry_files/ficheraCube.txt"
+        
+        if mesh_type in ['qu', 'quasi-uniform', 'quasi_uniform', 'quasiUniform']:
+        
+            h5_mesh_file = base_in_mesh_dir+"ficheraCube_qu/mesh_l%d.h5"%level
+            mfem_mesh_file = base_out_mesh_dir+"ficheraCube_qu/mesh_l%d.mesh"%level
+        
+        elif mesh_type in ['gr', 'graded']:
+            
+            h5_mesh_file = base_in_mesh_dir+"ficheraCube_gr/"+gr_type[deg-1]+"/mesh_l%d.h5"%level
+            mfem_mesh_file = base_out_mesh_dir+"ficheraCube_gr/"+gr_type[deg-1]+"/mesh_l%d.mesh"%level
+        
+        else:
+            print("Unknown mesh type!")
+            sys.exit()
+                
+    else:
+        
+        print("Unknown geometry type!")
+        sys.exit()
+        
     print(geom_file)
     print(h5_mesh_file)
     print(mfem_mesh_file)
     
     geom = gm.Geometry3d(geom_file)
-    preproc_mesh3d = Preprocess_mesh(geom)
-    preproc_mesh3d.convert_mesh_hdf5_to_mfem(h5_mesh_file, mfem_mesh_file)
-    
+    ppMesh = mconv.Hdf5ToMfemConverter(geom)
+    ppMesh.convert_mesh_hdf5_to_mfem(h5_mesh_file, mfem_mesh_file)
+
    
 if __name__=='__main__':
     
-    call_mesh_hdf5_to_mfem_3d()
-
+    n = len(sys.argv)
+    if n != 5:
+        print("Total arguments passed: ", n)
+        print("But 5 arguments required!")
+        sys.exit()
+    
+    geometry_type = str(sys.argv[1])
+    mesh_type = str(sys.argv[2])
+    level = int(sys.argv[3])
+    
+    deg = int(sys.argv[4])
+    if mesh_type in ['qu', 'quasi-uniform', 'quasi_uniform', 'quasiUniform']:
+        deg = 1
+    
+    # call converter
+    call_converter_hdf5_to_mfem_3d(geometry_type, mesh_type, level, deg)
+    
 
 # End of file
